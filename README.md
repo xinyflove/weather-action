99545d2803b8fcf5342025f8d36d4a030a683b3b
Caffrey Xin <xinyflove@sina.com>
2019-12-23 10:26:47 +0800

    测试看看

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # weather-action
 GitHub Actions 教程：定时发送天气邮件
+
+> 参考阮一峰老师的教程[GitHub Actions 教程：定时发送天气邮件 http://www.ruanyifeng.com/blog/2019/12/github_actions.html](http://www.ruanyifeng.com/blog/2019/12/github_actions.html)
---
+++ weather.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# 以调试的方式执行shell ，只识别定义过的变量，同时脚本传回值非0 直接结束shell
+set -eux
+
+CITY=Yantai
+LANGUAGE="zh-CN"
+UNIT=m
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+
+curl \
+  -H "Accept-Language: $LANGUAGE" \
+  -H "User-Agent: $UA" \
+  -o result.html \
+  wttr.in/$CITY?format=4\&$UNIT
