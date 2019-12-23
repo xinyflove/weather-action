7631fc8541f267beb1bfd53b56b10dd743d0a0f3
Caffrey Xin <xinyflove@sina.com>
2019-12-23 11:13:48 +0800

    修改返回天气格式

--- weather.sh
+++ weather.sh
@@ -8,9 +8,10 @@ CITY=Yantai
 LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+FORMAT=位置:%l+天气条件:%c+%C+温度:%t+湿度:%h+风速:%w+降水概率:%o
 
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o result.html \
-  wttr.in/$CITY?format=4\&$UNIT
+  wttr.in/$CITY?format=$FORMAT
